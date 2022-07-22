788a7a0bc1c13eb355de0ce5b69ea807018ec839
Erwan Cariou <erwan1.cariou@epitech.eu>
2022-07-22 16:41:10 +0200

    trying to ruin my code for super linter

--- main.c
+++ main.c
@@ -7,9 +7,11 @@
 
 #include <unistd.h>
 
+void Hello(int a){write(1, "I'm doing")}
+
 int main(int ac, char const * const av[])
 {
-    if (ac != 1 && av[1][1] != 'h')
+    if (ac != 1)
         return 84;
     write(1, "I'm a bird\n", 11);
     return 0;
